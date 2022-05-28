2a0ea33cb4a78a70a41c80491adb971185eb30df
Liuxiao6 <25676419+Liuxiao677@users.noreply.github.com>
2022-05-28 13:36:10 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,10 +39,10 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route','/🔞涩涩','/摸摸鱼研究室备份','/uploads'],
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route','/🔞涩涩','/摸摸鱼研究室备份','/uploads','/MPA资料'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: "",
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
